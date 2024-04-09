e38c2c55a6da12095f6b334f4903add26e92883e
[mrawlings2] <mrawlin6@uncc.edu>
2024-04-09 18:37:20 -0400

    Mini HeartAttack but no add event button now lol

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -4,7 +4,7 @@ import Fab from "../../components/Button/Fab";
 import Empty from "../../components/Empty/Empty";
 import GlobalContext from "../../Context/GlobalContext";
 import Row, { Col } from "../../Layout/Responsive";
-import AddEvent from "./AddEvent";
+//import AddEvent from "./AddEvent";
 import Event from "./Event";
 import "./events.css";
 import { eventsNavLinks } from "../../utils/navigation";
@@ -82,10 +82,10 @@ const Events = () => {
 					}}
 				/>
 			)}
-			<Fab icon="add" onClick={() => setShowAddEventBox(true)} />
+			{/*<Fab icon="add" onClick={() => setShowAddEventBox(true)} />
 			{showAddEventBox && (
-				<AddEvent close={() => setShowAddEventBox(false)} />
-			)}
+				//<AddEvent close={() => setShowAddEventBox(false)} />
+			)}*/}
 		</main>
 	);
 };
