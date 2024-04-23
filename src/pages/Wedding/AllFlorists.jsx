1dca01358593a96b2108336983e1c69cf30a58d6
Alish Jain <alishjain@Alishs-MacBook-Pro.local>
2024-04-23 18:16:56 -0400

    Update AllFlorists.jsx

--- src/pages/Wedding/AllFlorists.jsx
+++ src/pages/Wedding/AllFlorists.jsx
@@ -5,13 +5,14 @@ import "./wedding.css";
 
 const Florists = () => {
     const [filterCriteria, setFilterCriteria] = useState({
-        floristName: "",
-        location: "",
-        startingPrice: "",
-		review: ""
+        FloristName: "",
+        Location: "",
+        StartingPrice: "",
+		Review: ""
     });
 
     const floristsArray = Object.values(allFloristData);
+	const [showFilters, setShowFilters] = useState(false);
 
     const handleFilterChange = (event) => {
         const { name, value } = event.target;
@@ -20,27 +21,45 @@ const Florists = () => {
 
     const applyFilter = (florist) => {
         for (let key in filterCriteria) {
-            if (filterCriteria[key] !== "" && florist[key].toLowerCase().indexOf(filterCriteria[key].toLowerCase()) === -1) {
-                return false;
-            }
+            if (filterCriteria[key] !== "") {
+			const floristValue = florist[key]?.toString().toLowerCase(); // Convert to lowercase string
+				const filterValue = filterCriteria[key].toLowerCase();
+				if (key == 'FloristName' || key == 'Location') {
+					if (filterCriteria[key] !== "" && floristValue.indexOf(filterValue) === -1) {
+						return false;
+					}
+				} else if (key == 'Review') {
+					let v = +floristValue;
+					let f = +filterValue;
+					if (!floristValue || v < f) {
+						return false;
+					}
+				}
+		}
         }
         return true;
     };
 
     return (
         <div className="wedding-container">
-            <div className="blockss">
-                {/* Florists */}
-                <div className="list-item" style={{ backgroundImage: `url(${florist})` }}>
-                    <p className="category-title">Florists</p>
-                </div>
-                <div className="vendor-details">
-                    {/* Filter section */}
-                    <div className="filter-section">
-                        <input type="text" name="floristName" placeholder="Florist Name" value={filterCriteria.floristName} onChange={handleFilterChange} />
-                        <input type="text" name="location" placeholder="Location" value={filterCriteria.location} onChange={handleFilterChange} />
-                        <input type="text" name="startingPrice" placeholder="Starting Price" value={filterCriteria.startingPrice} onChange={handleFilterChange} />
-						<input type="text" name="review" placeholder="Review" value={filterCriteria.review} onChange={handleFilterChange} />
+			<div style={{
+				height: '30px',
+				display: 'flex',
+				justifyContent: 'right'
+			}}>
+				<button onClick={() => setShowFilters(!showFilters)} style={{
+					fontWeight: '600',
+					backgroundColor: 'cadetblue',
+					width: '10vw',
+					height: '30px'
+				}}>
+					Filters
+				</button>
+				{showFilters && (
+			 <div className="filter-section">
+                        <input type="text" name="FloristName" placeholder="Florist Name" value={filterCriteria.FloristName} onChange={handleFilterChange} />
+                        <input type="text" name="Location" placeholder="Location" value={filterCriteria.Location} onChange={handleFilterChange} />
+						<input type="text" name="Review" placeholder="Review" value={filterCriteria.Review} onChange={handleFilterChange} />
                         {/* Add more inputs for other fields */}
                         <button onClick={() => setFilterCriteria({
                             floristName: "",
@@ -50,6 +69,16 @@ const Florists = () => {
                             // Reset other fields here
                         })}>Reset</button>
                     </div>
+					)}
+					</div>
+            <div className="blockss">
+                {/* Florists */}
+                <div className="list-item" style={{ backgroundImage: `url(${florist})` }}>
+                    <p className="category-title">Florists</p>
+                </div>
+                <div className="vendor-details">
+                    {/* Filter section */}
+
                     {/* Florist details based on filter */}
                     {floristsArray.length > 0 ? (
                         floristsArray.filter(applyFilter).map((florist, index) => (
